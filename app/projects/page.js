20cd771954a3633cf85f0e95fbb7c0830cb5c421
Markus Wiland <46526900+MarkusWiland@users.noreply.github.com>
2023-11-28 12:57:59 +0000

    working on card

--- app/projects/page.js
+++ app/projects/page.js
@@ -27,6 +27,9 @@ export default function Projects() {
       {dataa.map((card, index) => (
         <Card key={index} card={card} index={index} leng={dataa.length} />
       ))}
+        <div className="absolute top-[80vh] left-1/2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-down"><path d="M8 18L12 22L16 18"/><path d="M12 2V22"/></svg>
+      </div>
     </main>
   );
 }
--- components/projects/Card.js
+++ components/projects/Card.js
@@ -14,7 +14,7 @@ export default function Card({ card, index, leng }) {
   return (
     <main
       ref={container}
-      className="h-[100vh] sticky top-0  flex items-center justify-center"
+      className="h-[100vh] sticky top-0  flex  items-center justify-center"
     >
       <motion.div
         style={{
@@ -23,17 +23,17 @@ export default function Card({ card, index, leng }) {
         }}
         className={`flex flex-col relative w-full  origin-top	 bg-box py-4 px-8 border border-opacity-10 border-white rounded-md`}
       >
-        <h1 className="text-2xl">{title}</h1>
+        <h1 className="text-2xl capitilize pb-4">{title}</h1>
         <h3 className="text-sm uppercase">PERSONAL PROJECT</h3>
         <div className="grid grid-cols-[3fr_2fr] gap-x-[30px] h-full mt-[25px]">
-          <div className="pt-[25px] pb-[50px] px-0 border-t-[#d9d9d9] border-t border-solid ">
-            <h4>Descriptipn</h4>
+          <div className="flex flex-col gap-6 pt-[25px] pb-[50px] px-0 border-t-[#d9d9d9] border-t border-solid ">
+            <h4 className="font-bold uppercase">Descriptipn</h4>
             <p className="text-md">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industrys standard dummy text
             </p>
             <div>
-              <h4>Tech used</h4>
+              <h4 className="font-bold uppercase">Tech used</h4>
               <div className="flex items-center flex-wrap">
                 <div>
                   <span>Next JS</span>
@@ -55,13 +55,24 @@ export default function Card({ card, index, leng }) {
                 </div>
               </div>
             </div>
-            <span>hejsan</span>
+        
           </div>
-          <div>
-            <Image src={img} alt={title} width={55} height={55} />
+          <div className="relative flex flex-col gap-4 items-center justify-center" style={{backgroundImage: `url(${img})`}}>
+            <div className="w-full h-full absolute z-[6] opacity-40 left-0 top-0 bg-black"/>
+            <div className="bg-white z-[7] relative flex flex-col items-center justify-center w-[90%] max-w-[140px] h-[100px] text-center text-[black] text-sm font-medium no-underline transition-[0.3s] duration-[ease]">
+
+            <span>icon</span>
+           <p>live project</p>
+              </div>
+              <div className="bg-white z-[7] relative flex flex-col items-center justify-center w-[90%] max-w-[140px] h-[100px] text-center text-[black] text-sm font-medium no-underline transition-[0.3s] duration-[ease]">
+
+              <span>icon</span>
+           <p>live project</p>
+              </div>
           </div>
         </div>
       </motion.div>
+    
     </main>
   );
 }
